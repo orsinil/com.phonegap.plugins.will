1f1b4403ddbe1e8e4db2bc58f9bc33d7617b562e
Luca Orsini <orsinil@inwind.it>
2014-11-17 12:46:28 +0100

    1

--- www/will.js
+++ www/will.js
@@ -1,3 +1,11 @@
+/**
+ * cordova is available under *either* the terms of the modified BSD license *or* the
+ * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
+ *
+ * Copyright (c) Matt Kane 2010
+ * Copyright (c) 2011, IBM Corporation
+ */
+
 var willPlugin = {
     open: function(successCallback, errorCallback) {
 		 cordova.exec(
