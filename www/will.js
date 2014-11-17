313000927380f0642dd349161f0968a9fe0d45f6
Luca Orsini <orsinil@inwind.it>
2014-11-17 13:15:55 +0100

    1

--- www/will.js
+++ www/will.js
@@ -1,18 +1,20 @@
-/**
+ /**
  * cordova is available under *either* the terms of the modified BSD license *or* the
  * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
  *
  * Copyright (c) Matt Kane 2010
  * Copyright (c) 2011, IBM Corporation
  */
+var WillPlugin = function() {
+};
 
-var willPlugin = {
-    open: function(successCallback, errorCallback) {
+WillPlugin.open =  function(successCallback, errorCallback) {
 		 cordova.exec(
 					successCallback, // success callback function
 					errorCallback, // error callback function
 					'willPlugin', // mapped to our native Java class called "CalendarPlugin"
 					'addWillEntry'
 				); 
-     }
-}
+     };
+
+module.exports = WillPlugin;
