b3fdca89f41af8fbd9e12d005a91b814a0ed9054
Luca Orsini <orsinil@inwind.it>
2014-11-17 10:45:21 +0100

    1

--- www/will.js
+++ www/will.js
@@ -1,35 +1,11 @@
-    var willPluginLoader(require, exports, module) {
-
-        var exec = require("cordova/exec");
-
-
-
-        WillPlugin.prototype.scan = function (successCallback, errorCallback) {
-            if (errorCallback == null) {
-                errorCallback = function () {
-                };
-            }
-
-            if (typeof errorCallback != "function") {
-                console.log("willPlugin.open failure: failure parameter not a function");
-                return;
-            }
-
-            if (typeof successCallback != "function") {
-                console.log("willPlugin.open failure: success callback parameter must be a function");
-                return;
-            }
-
-            exec(successCallback, errorCallback, 'willPlugin', 'open', []);
-        };
-
-        //-------------------------------------------------------------------
-
-        var willPlugin = new WillPlugin();
-        module.exports = willPlugin;
+var willPlugin = {
+    open: function(successCallback, errorCallback) {
+		 cordova.exec(
+					successCallback, // success callback function
+					errorCallback, // error callback function
+					'willPlugin', // mapped to our native Java class called "CalendarPlugin"
+					'addWillEntry'
+				); 
 
     }
-
-    willPluginLoader(require, exports, module);
-
-    cordova.define("cordova/plugin/WillPlugin", willPluginLoader);
+}
