ee0509b512a8380fcfc72174318f7d27d8306c50
Luca Orsini <orsinil@inwind.it>
2014-11-26 12:47:27 +0100

    1

--- src/android/LibraryProject/src/com/wacom/toolsconfigurator/Controller.java
+++ src/android/LibraryProject/src/com/wacom/toolsconfigurator/Controller.java
@@ -182,7 +182,7 @@ public class Controller{
         	saveasbitmap();
         	 break;
          case R.id.btn_close:
-        	 //codice per chiudere
+        	 finish();
          default:
             
      }
